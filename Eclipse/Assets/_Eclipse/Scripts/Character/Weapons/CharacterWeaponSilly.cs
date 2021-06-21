f1720ed093c6a48335d1cde100914710213b0de2
Chris Butterworth <chris.butterworth@mightykingdom.com>
2021-06-21 12:07:49 +0930

    Reworked the anim controller. Added materials that we can edit for the main character. Tweaked the main attack.

--- Eclipse/Assets/_Eclipse/Scripts/Character/Weapons/CharacterWeaponSilly.cs
+++ Eclipse/Assets/_Eclipse/Scripts/Character/Weapons/CharacterWeaponSilly.cs
@@ -6,20 +6,26 @@ using DG.Tweening;
 public class CharacterWeaponSilly : CharacterWeapon
 {
     [SerializeField] private float primaryAttackDistance;
+    [SerializeField] private float primaryAttackSlideTime;
     
     public override void StartPrimaryAttack(Vector3 clickPoint)
     {
         base.StartPrimaryAttack(clickPoint);
         
         characterMasterController.transform.LookAt(clickPoint);
-        characterMasterController.CharacterMovementController.LastDirection = characterMasterController.transform.rotation;
+
+        float slideDistance = Mathf.Min(Vector3.Distance(transform.position, clickPoint), primaryAttackDistance);
         
-        characterMasterController.transform.DOJump(clickPoint, 4f, 1, 0.6f).OnComplete(AttackComplete);
+        characterMasterController.CharacterMovementController.LastDirection = characterMasterController.transform.rotation;
+
+        Vector3 finalPos = transform.position + ((clickPoint - transform.position).normalized * slideDistance);
+        
+        characterMasterController.transform.DOMove(finalPos, primaryAttackSlideTime).OnComplete(AttackComplete);
     }
 
-    public override void StartSecondaryAttack(Vector3 hitPoint)
+    public override void StartSecondaryAttack(Vector3 clickPoint)
     {
-        base.StartSecondaryAttack(hitPoint);
+        base.StartSecondaryAttack(clickPoint);
         
         AttackComplete();
     }
